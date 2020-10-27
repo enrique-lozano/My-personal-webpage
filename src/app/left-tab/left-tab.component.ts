97ad3a177b630282ddd61a97dce79e3eaafa579b
enriqueloz88 <kikelozano8@gmail.com>
2020-10-27 10:33:15 +0100

    🎉 Initial commit

---
+++ src/app/left-tab/left-tab.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import * as myGlobals from '../../globals'; //Global variables
+import { TabHeaderComponent } from '../tab-header/tab-header.component';
+import {RightTabComponent} from '../right-tab/right-tab.component'
+
+@Component({
+  selector: 'app-left-tab',
+  templateUrl: './left-tab.component.html',
+  providers: [TabHeaderComponent, RightTabComponent],
+  styleUrls: ['./left-tab.component.scss']
+})
+export class LeftTabComponent implements OnInit {
+
+  isCheckedDark = true;
+  isChecked2 = false;
+
+  //Importing the globals variables
+  name = myGlobals.name;
+  job = myGlobals.job;
+
+  constructor(public tabHeader:TabHeaderComponent, public rightTab:RightTabComponent) { }
+
+  ngOnInit(): void {
+  }
+
+  openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+  
+  closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
+
+  toDark(){
+    if(this.isCheckedDark == true){ //Dark
+      //We change the right tab, the headers and the left tab
+      console.log("Dark mode.....");
+      this.rightTab.darkModeRight();
+      this.tabHeader.toDark();
+      document.getElementById("main-tab").style.backgroundColor = "#151C21";
+    } else{ //Light
+      //We change the right tab, the headers and the left tab
+      console.log("Light mode.....");
+      this.tabHeader.toLight();
+      this.rightTab.lightModeRight();
+      document.getElementById("main-tab").style.backgroundColor = "#3248F4";
+    }
+   
+  }
+  
+  language(){
+  }
+
+
+
+}
---
+++ src/globals.ts
@@ -0,0 +1,9 @@
+//
+// ===== File globals.ts    
+//
+'use strict';
+
+export const name: string="Enrique Lozano";
+export const job: string="Ingeniero Informático";
+export const description: string="Estudiante de Ingeniería Informática. Me considero una persona abierta y sociable, con una gran pasión por la tecnología y muchas ganas de aprender nuevos conocimientos y vivir nuevas experiencias.";
+export const about_description: string="en esta sección podras consultar otros datos de interés sobre mi, asi como consultar mi CV. Sientete libre de navegar por esta página web y de ponerte en contacto conmigo si te interesa :)"
