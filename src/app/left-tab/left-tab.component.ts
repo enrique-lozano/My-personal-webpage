4bed130d4d930735a67ea6a12d9846c866af2be0
Enrique Lozano <61509169+enriqueloz88@users.noreply.github.com>
2021-02-01 14:43:42 +0100

    ✨ Night Mode Button

--- src/app/left-tab/left-tab.component.ts
+++ src/app/left-tab/left-tab.component.ts
@@ -1,56 +1,61 @@
-import { Component, OnInit } from '@angular/core';
-import * as myGlobals from '../../globals'; //Global variables
-import { TabHeaderComponent } from '../tab-header/tab-header.component';
-import {RightTabComponent} from '../right-tab/right-tab.component'
+import { Component, OnInit } from "@angular/core";
+import * as myGlobals from "../../globals"; //Global variables
+import { TabHeaderComponent } from "../tab-header/tab-header.component";
+import { RightTabComponent } from "../right-tab/right-tab.component";
+import { TranslateService } from "@ngx-translate/core";
 
 @Component({
-  selector: 'app-left-tab',
-  templateUrl: './left-tab.component.html',
-  providers: [TabHeaderComponent, RightTabComponent],
-  styleUrls: ['./left-tab.component.scss']
+    selector: "app-left-tab",
+    templateUrl: "./left-tab.component.html",
+    providers: [TabHeaderComponent, RightTabComponent],
+    styleUrls: ["./left-tab.component.scss"],
 })
 export class LeftTabComponent implements OnInit {
+    isCheckedDark = true;
+    isChecked2 = false;
 
-  isCheckedDark = true;
-  isChecked2 = false;
+    //Importing the globals variables
+    name = myGlobals.name;
+    job = myGlobals.job;
 
-  //Importing the globals variables
-  name = myGlobals.name;
-  job = myGlobals.job;
-
-  constructor(public tabHeader:TabHeaderComponent, public rightTab:RightTabComponent) { }
-
-  ngOnInit(): void {
-  }
-
-  openNav() {
-    document.getElementById("myNav").style.width = "100%";
-  }
-  
-  closeNav() {
-    document.getElementById("myNav").style.width = "0%";
-  }
-
-  toDark(){
-    if(this.isCheckedDark == true){ //Dark
-      //We change the right tab, the headers and the left tab
-      console.log("Dark mode.....");
-      this.rightTab.darkModeRight();
-      this.tabHeader.toDark();
-      document.getElementById("main-tab").style.backgroundColor = "#151C21";
-    } else{ //Light
-      //We change the right tab, the headers and the left tab
-      console.log("Light mode.....");
-      this.tabHeader.toLight();
-      this.rightTab.lightModeRight();
-      document.getElementById("main-tab").style.backgroundColor = "#3248F4";
+    constructor(
+        public tabHeader: TabHeaderComponent,
+        public rightTab: RightTabComponent,
+        public translate: TranslateService
+    ) {
+        translate.addLangs(["es", "en"]);
+        translate.setDefaultLang("es");
     }
-   
-  }
-  
-  language(){
-  }
 
+    ngOnInit(): void {}
 
+    openNav() {
+        document.getElementById("myNav").style.width = "100%";
+    }
 
+    closeNav() {
+        document.getElementById("myNav").style.width = "0%";
+    }
+
+    toDark() {
+        if (this.isCheckedDark == true) {
+            //Dark
+            //We change the right tab, the headers and the left tab
+            console.log("Dark mode.....");
+            this.rightTab.darkModeRight();
+            this.tabHeader.toDark();
+            document.getElementById("main-tab").style.backgroundColor =
+                "#151C21";
+        } else {
+            //Light
+            //We change the right tab, the headers and the left tab
+            console.log("Light mode.....");
+            this.tabHeader.toLight();
+            this.rightTab.lightModeRight();
+            document.getElementById("main-tab").style.backgroundColor =
+                "#3248F4";
+        }
+    }
+
+    language() {}
 }
--- src/app/right-tab/right-tab.component.ts
+++ src/app/right-tab/right-tab.component.ts
@@ -1,94 +1,97 @@
-import { Component, OnInit, Renderer2 } from '@angular/core';
-import * as myGlobals from '../../globals'; //Global variables
-import {MatDialog} from '@angular/material/dialog';
-import {FormControl, Validators} from '@angular/forms';
-import { TranslateService } from '@ngx-translate/core';
+import { Component, OnInit, Renderer2 } from "@angular/core";
+import * as myGlobals from "../../globals"; //Global variables
+import { MatDialog } from "@angular/material/dialog";
+import { FormControl, Validators } from "@angular/forms";
+import { TranslateService } from "@ngx-translate/core";
 
 @Component({
-  selector: 'app-right-tab',
-  templateUrl: './right-tab.component.html',
-  styleUrls: ['./right-tab.component.scss']
+    selector: "app-right-tab",
+    templateUrl: "./right-tab.component.html",
+    styleUrls: ["./right-tab.component.scss"],
 })
 export class RightTabComponent implements OnInit {
+    emailFormControl = new FormControl("", [
+        Validators.required,
+        Validators.email,
+    ]);
 
-  emailFormControl = new FormControl('', [
-    Validators.required,
-    Validators.email,
-  ]);
+    name = myGlobals.name;
+    job = myGlobals.job;
+    description = myGlobals.description;
+    about_description = myGlobals.about_description;
 
-  name = myGlobals.name;
-  job = myGlobals.job;
-  description = myGlobals.description;
-  about_description = myGlobals.about_description;
-  
-  //Binding variables
-  public about = "Sobre mi"
-  public education = "Educación"
-  public skills = "Habilidades"
-  public rewards = "Reconocimientos"
-  public experience = "Experiencia"
-  public contact = "Contacto"
-  public main_skills = "Principales habilidades"
+    //Binding variables
+    public about = "Sobre mi";
+    public education = "Educación";
+    public skills = "Habilidades";
+    public rewards = "Reconocimientos";
+    public experience = "Experiencia";
+    public contact = "Contacto";
+    public main_skills = "Principales habilidades";
 
-  //Varibles for the form
-  public email:string;
-  public name_email:string;
-  public subject:string;
-  public text:string;
+    //Varibles for the form
+    public email: string;
+    public name_email: string;
+    public subject: string;
+    public text: string;
 
-  public selectLang:string;
-
-
-  constructor(public dialog: MatDialog,  private renderer: Renderer2, public translate: TranslateService) { 
-    translate.addLangs(['es', 'en']);
-    translate.setDefaultLang('es');
-  }
-
-  switchLang(lang: string) { //Translation
-    this.translate.use(lang);
-    this.selectLang = lang;
-  }
-
-  //Function to open the popup
-  openDialog(variable: number) {
-    const dialogRef = null;
-    if(variable == 1){
-      const dialogRef = this.dialog.open(DialogContentExampleDialog);
-    } else if (variable == 2){
-      const dialogRef = this.dialog.open(DialogContentExampleDialog2);
-    } else{
-      alert("Popup not found");
+    constructor(
+        public dialog: MatDialog,
+        private renderer: Renderer2,
+        public translate: TranslateService
+    ) {
+        translate.addLangs(["es", "en"]);
+        translate.setDefaultLang("es");
     }
 
-    dialogRef.afterClosed().subscribe(result => {
-      console.log(`Dialog result: ${result}`);
-    });
-  }
-
-  //Functions that implements the dark mode on this
-  darkModeRight(){
-    // Change background
-    this.renderer.setStyle(document.body, 'background-color', "#000000");
-    // Change text
-    document.getElementById("container").style.color = "white";
-    //Change inicio
-    document.getElementById("inicio").style.backgroundColor = "#151C21";
-    //Change sobre
-    document.getElementById("sobre").style.backgroundColor = "#151C21";
-    //Change educacion
-    document.getElementById("educacion").style.backgroundColor = "#151C21";
-    //Change habilidades1
-    document.getElementById("habilidades1").style.backgroundColor = "#151C21";
-    //Change habilidades
-    document.getElementById("habilidades").style.backgroundColor = "#151C21";
-    var skill = document.getElementsByClassName("skill-set") as HTMLCollectionOf<HTMLElement>;
-    for(var i=0, len=skill.length; i<len; i++)
-    { 
-        skill[i].style.backgroundColor = "black";
-        skill[i].style.color = "white";
+    switchLang(lang: string) {
+        //Translation
+        this.translate.use(lang);
     }
-    //Change reconocimientos
-    /*
+
+    //Function to open the popup
+    openDialog(variable: number) {
+        const dialogRef = null;
+        if (variable == 1) {
+            const dialogRef = this.dialog.open(DialogContentExampleDialog);
+        } else if (variable == 2) {
+            const dialogRef = this.dialog.open(DialogContentExampleDialog2);
+        } else {
+            alert("Popup not found");
+        }
+
+        dialogRef.afterClosed().subscribe((result) => {
+            console.log(`Dialog result: ${result}`);
+        });
+    }
+
+    //Functions that implements the dark mode on this
+    darkModeRight() {
+        // Change background
+        this.renderer.setStyle(document.body, "background-color", "#000000");
+        // Change text
+        document.getElementById("container").style.color = "white";
+        //Change inicio
+        document.getElementById("inicio").style.backgroundColor = "#151C21";
+        //Change sobre
+        document.getElementById("sobre").style.backgroundColor = "#151C21";
+        //Change educacion
+        document.getElementById("educacion").style.backgroundColor = "#151C21";
+        //Change habilidades1
+        document.getElementById("habilidades1").style.backgroundColor =
+            "#151C21";
+        //Change habilidades
+        document.getElementById("habilidades").style.backgroundColor =
+            "#151C21";
+        var skill = document.getElementsByClassName(
+            "skill-set"
+        ) as HTMLCollectionOf<HTMLElement>;
+        for (var i = 0, len = skill.length; i < len; i++) {
+            skill[i].style.backgroundColor = "black";
+            skill[i].style.color = "white";
+        }
+        //Change reconocimientos
+        /*
     document.getElementById("reconocimientos").style.backgroundColor = "#151C21";
     var rewards = document.getElementsByClassName("front") as HTMLCollectionOf<HTMLElement>;
     for(var i=0, len=rewards.length; i<len; i++)
@@ -102,41 +105,44 @@ export class RightTabComponent implements OnInit {
       rewards[i].style.backgroundColor = "black";
       rewards[i].style.color = "white";
     }*/
-    //Change experiencia
-    document.getElementById("experiencia").style.backgroundColor = "#151C21";
-    //Change contacto
-    document.getElementById("contacto").style.backgroundColor = "#151C21";
-    document.getElementById("light-form").style.display = "none";
-    document.getElementById("dark-form").style.display = "block";
-    //document.getElementById("mat-form-field").style.backgroundColor = "white";
-    //Change left-tab
-    //this.leftTab.toDark();
-    //this.tabHeader.toDark();
-  }
-
-  lightModeRight(){
-    // Change background
-    this.renderer.setStyle(document.body, 'background-color', "#F3F6FF");
-    // Change text
-    document.getElementById("container").style.color = "black";
-    //Change inicio
-    document.getElementById("inicio").style.backgroundColor = "#3248F4";
-    //Change sobre
-    document.getElementById("sobre").style.backgroundColor = "white";
-    //Change educacion
-    document.getElementById("educacion").style.backgroundColor = "white";
-    //Change habilidades1
-    document.getElementById("habilidades1").style.backgroundColor = "#3248F4";
-    //Change habilidades
-    document.getElementById("habilidades").style.backgroundColor = "white";
-    var skill = document.getElementsByClassName("skill-set") as HTMLCollectionOf<HTMLElement>;
-    for(var i=0, len=skill.length; i<len; i++)
-    { 
-        skill[i].style.backgroundColor = "white";
-        skill[i].style.color = "black";
+        //Change experiencia
+        document.getElementById("experiencia").style.backgroundColor =
+            "#151C21";
+        //Change contacto
+        document.getElementById("contacto").style.backgroundColor = "#151C21";
+        document.getElementById("light-form").style.display = "none";
+        document.getElementById("dark-form").style.display = "block";
+        //document.getElementById("mat-form-field").style.backgroundColor = "white";
+        //Change left-tab
+        //this.leftTab.toDark();
+        //this.tabHeader.toDark();
     }
-    //Change reconocimientos
-    /*
+
+    lightModeRight() {
+        // Change background
+        this.renderer.setStyle(document.body, "background-color", "#F3F6FF");
+        // Change text
+        document.getElementById("container").style.color = "black";
+        //Change inicio
+        document.getElementById("inicio").style.backgroundColor = "#3248F4";
+        //Change sobre
+        document.getElementById("sobre").style.backgroundColor = "white";
+        //Change educacion
+        document.getElementById("educacion").style.backgroundColor = "white";
+        //Change habilidades1
+        document.getElementById("habilidades1").style.backgroundColor =
+            "#3248F4";
+        //Change habilidades
+        document.getElementById("habilidades").style.backgroundColor = "white";
+        var skill = document.getElementsByClassName(
+            "skill-set"
+        ) as HTMLCollectionOf<HTMLElement>;
+        for (var i = 0, len = skill.length; i < len; i++) {
+            skill[i].style.backgroundColor = "white";
+            skill[i].style.color = "black";
+        }
+        //Change reconocimientos
+        /*
     document.getElementById("reconocimientos").style.backgroundColor = "white";
     var rewards = document.getElementsByClassName("front") as HTMLCollectionOf<HTMLElement>;
     for(var i=0, len=rewards.length; i<len; i++)
@@ -151,27 +157,25 @@ export class RightTabComponent implements OnInit {
       rewards[i].style.color = "black";
     }
     */
-    //Change experiencia
-    document.getElementById("experiencia").style.backgroundColor = "white";
-    //Change contacto
-    document.getElementById("contacto").style.backgroundColor = "white";
-    document.getElementById("light-form").style.display = "block";
-    document.getElementById("dark-form").style.display = "none";
-  }
+        //Change experiencia
+        document.getElementById("experiencia").style.backgroundColor = "white";
+        //Change contacto
+        document.getElementById("contacto").style.backgroundColor = "white";
+        document.getElementById("light-form").style.display = "block";
+        document.getElementById("dark-form").style.display = "none";
+    }
 
-  ngOnInit(): void {
-  }
-
+    ngOnInit(): void {}
 }
 
 @Component({
-  selector: 'dialog-content-example-dialog',
-  templateUrl: 'dialog-content-example-dialog.html',
+    selector: "dialog-content-example-dialog",
+    templateUrl: "dialog-content-example-dialog.html",
 })
 export class DialogContentExampleDialog {}
 
 @Component({
-  selector: 'dialog-content-example-dialog-2',
-  templateUrl: 'dialog-content-example-dialog-2.html',
+    selector: "dialog-content-example-dialog-2",
+    templateUrl: "dialog-content-example-dialog-2.html",
 })
 export class DialogContentExampleDialog2 {}
