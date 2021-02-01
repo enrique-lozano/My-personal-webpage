76c14e1adcdfe75f941f165bcdba7696da639738
Enrique Lozano <61509169+enriqueloz88@users.noreply.github.com>
2021-02-01 20:00:10 +0100

    🚀 Hosting updated

--- dist/LozanoWebPage/polyfills-es2015.js
+++ dist/LozanoWebPage/polyfills-es2015.js
@@ -3033,7 +3033,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Users\kikel\Desktop\LozanoWebPage\src\polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! C:\Users\kikel\Documents\GitHub\My-personal-webpage\src\polyfills.ts */"./src/polyfills.ts");
 
 
 /***/ })
