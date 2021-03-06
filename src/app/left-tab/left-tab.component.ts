af9fe464fc1cf8a11574778a3c3535509951a1dd
Enrique Lozano <61509169+enriqueloz88@users.noreply.github.com>
2021-03-06 15:39:24 +0100

    🌐 Translation improvements

--- src/app/left-tab/left-tab.component.ts
+++ src/app/left-tab/left-tab.component.ts
@@ -1,18 +1,14 @@
 import { Component, OnInit } from "@angular/core";
-import { TabHeaderComponent } from "../tab-header/tab-header.component";
 import { TranslateService } from "@ngx-translate/core";
 
 @Component({
     selector: "app-left-tab",
     templateUrl: "./left-tab.component.html",
-    providers: [TabHeaderComponent],
+    providers: [],
     styleUrls: ["./left-tab.component.scss"],
 })
 export class LeftTabComponent implements OnInit {
-    constructor(
-        public tabHeader: TabHeaderComponent,
-        public translate: TranslateService
-    ) {
+    constructor(public translate: TranslateService) {
         translate.addLangs(["es", "en"]);
         translate.setDefaultLang("es");
     }
@@ -20,12 +16,10 @@ export class LeftTabComponent implements OnInit {
     ngOnInit(): void {}
 
     toDark() {
-        this.tabHeader.toDark();
         document.getElementById("main-tab").style.backgroundColor = "#151C21";
     }
 
     toLight() {
-        this.tabHeader.toLight();
         document.getElementById("main-tab").style.backgroundColor = "#3248F4";
     }
 }
