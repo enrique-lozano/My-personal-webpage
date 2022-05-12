628cb530ed00ea96f027942006387a02fdc2ca61
Enrique Lozano <61509169+enriqueloz88@users.noreply.github.com>
2022-05-12 14:00:21 +0200

    💄 Improved margins & paddings in some components

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="flex w-full items-center flex-col-reverse md:flex-row">
 		<div class="w-full text-center md:w-2/5 mt-3 md:mt-0">
-			<img style="max-height: 30vh" class="pr-4" src="../assets/illustrations/coder.svg" alt="" />
+			<img style="max-height: 30vh" class="pr-4 py-6" src="../assets/illustrations/coder.svg" alt="" />
 		</div>
 		<div class="w-full md:w-3/5">
 			<h2 class="mb-3">{{ $t('aboutMe.title') }}</h2>
